6e737f6b25b44130a3510a88cc8dbe1d594314e9
abirtarchoun <abirtarchoun.8@gmail.com>
2021-11-13 23:27:19 +0100

    Multiply the card into a collection

--- lib/home.dart
+++ lib/home.dart
@@ -18,13 +18,43 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+      count,
+          (int index) => Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset('assets/diamond.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const <Widget>[
+                  Text('Title'),
+                  SizedBox(height: 8.0),
+                  Text('Secondary Text'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return cards;
+  }
   // TODO: Add a variable for Category (104)
 
   @override
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return  Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
       appBar: AppBar(
         leading: IconButton(
@@ -65,38 +95,14 @@ class HomePage extends StatelessWidget {
       ),
       // TODO: Add a grid view (102)
       // TODO: Add a grid view (102)
+      // TODO: Add a grid view (102)
       body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        // TODO: Build a grid of cards (102)
-    children: <Widget>[
-    Card(
-    clipBehavior: Clip.antiAlias,
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-    AspectRatio(
-    aspectRatio: 18.0 / 11.0,
-    child: Image.asset('assets/diamond.png'),
-    ),
-    Padding(
-    padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: const <Widget>[
-    Text('Title'),
-    SizedBox(height: 8.0),
-    Text('Secondary Text'),
-    ],
-    ),
-    ),
-    ],
-    ),
-    )
-    ],
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(16.0),
+          childAspectRatio: 8.0 / 9.0,
+          children: _buildGridCards(10) // Replace
       ),
-      resizeToAvoidBottomInset: false,
     );
   }
+
 }
